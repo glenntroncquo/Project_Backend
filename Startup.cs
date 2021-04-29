9503ba554ad7c300bfad3c025380f8d4da03df3b
glenntroncquo <glenntroncquo1@gmail.com>
2021-04-29 12:47:36 +0200

    Configuration Added

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Project_Backend.Configuration;
+using Project_Backend.data;
 
 namespace Project_Backend
 {
@@ -26,6 +28,8 @@ namespace Project_Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ConnectionStrings>(Configuration.GetSection("ConnectionStrings"));
+            services.AddDbContext<ProjectBackendContext>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
---
+++ data/ProjectBackendContext.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Project_Backend.Configuration;
+
+namespace Project_Backend.data
+{
+    public class ProjectBackendContext : DbContext
+    {
+        private ConnectionStrings _connectionstrings;
+        public ProjectBackendContext(DbContextOptions options, IOptions<ConnectionStrings> connectionstrings) 
+        : base(options)
+        {
+            _connectionstrings = connectionstrings.Value;
+        }
+    }
+}
